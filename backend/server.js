88c4cb2b04a499282ee88e1b051961af9823f558
Mahi Sawner <mahisawner1234@gmail.com>
2026-04-26 11:28:11 +0530

    Changes

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,16 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
